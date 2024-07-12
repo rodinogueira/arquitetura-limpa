4b220e9915782e951148820b78732942bad1d34f
Rodrigo Nogueira <rdg6design@gmail.com>
2024-07-12 06:10:18 -0300

    feat: implementa boundaries em useCase

--- src/application/useCases/GetTasks.ts
+++ src/application/useCases/GetTasks.ts
@@ -1,9 +1,25 @@
+// src/application/useCases/GetTasks.ts
+import { GetTasksInputBoundary } from './boundaries/GetTasksInputBoundary';
+import { GetTasksOutputBoundary } from './boundaries/GetTasksOutputBoundary';
 import { TaskRepository } from '../../domain/repositories/TaskRepository';
+import { TaskDTO } from '../dtos/TaskDTO';
 
-export class GetTasks {
-  constructor(private taskRepository: TaskRepository) {}
+export class GetTasks implements GetTasksInputBoundary {
+  constructor(
+    private taskRepository: TaskRepository,
+    private outputBoundary: GetTasksOutputBoundary
+  ) {}
 
-  execute = async () => {
-    return await this.taskRepository.getTasks();
-  };
+  async execute(): Promise<void> {
+    try {
+      const tasks: TaskDTO[] = await this.taskRepository.getTasks();
+      this.outputBoundary.presentSuccess(tasks);
+    } catch (error) {
+      if (error instanceof Error) {
+        this.outputBoundary.presentError(error);
+      } else {
+        this.outputBoundary.presentError(new Error('Unknown error occurred'));
+      }
+    }
+  }
 }
